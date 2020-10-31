3d202f476a8e631fabbbe349162b2ae68e9ca187
Adam Olech <nddr89@gmail.com>
2020-10-31 14:30:02 +0100

    Commit some vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    </b-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { BNavbar } from 'bootstrap-vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'b-navbar': BNavbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
