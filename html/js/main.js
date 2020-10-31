987cb5b7d6d431d4baab9a28a90f46fdc6273561
Adam Olech <nddr89@gmail.com>
2020-10-31 19:27:53 +0100

    Basic table for tracks

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,31 @@
 var apiHelper = {
   currPage: 0,
   apiBaseUrl: 'http://localhost:6789/api',
+  transform: {
+    tag: 'tr',
+    children: [
+      {
+        'tag':'td',
+        'html':'${id}',
+      },
+      {
+        'tag':'td',
+        'html':'${artist}',
+      },
+      {
+        'tag':'td',
+        'html':'${album}',
+      },
+      {
+        'tag':'td',
+        'html':'${name}',
+      },
+      {
+        'tag':'td',
+        'html':'${played_at}',
+      },
+    ],
+  },
   tracks: {},
   getTracks: function() {
     var r = new XMLHttpRequest();
@@ -9,9 +34,15 @@ var apiHelper = {
       if(r.readyState == 4) {
         if(r.status == 200) {
           this.tracks = JSON.parse(r.responseText);
+          $('#tracks tbody').remove();
+          $('#tracks').json2html(this.tracks, this.transform);
         }
       }
     }.bind(this);
     r.send(null);
   },
 };
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    apiHelper.getTracks();
+});
